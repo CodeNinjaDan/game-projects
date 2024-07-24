33bcfbb11da41fff4d0b5596331f0c2c9ac9e241
Dan <dnlmuiru@gmail.com>
2024-07-24 23:11:08 +0300

    Create py4e.py

---
+++ Projects/py4e.py
@@ -0,0 +1,51 @@
+name = input("who are you ?")
+SyntaxError: invalid syntax. Perhaps you forgot a comma?
+name = input('Who are you ?')
+Who are you ?
+Daniel Kirumbi
+SyntaxError: incomplete input
+name = input ('Who are you ?')
+Who are you ?Daniel Kirumbi
+print ('Welcome',name)
+Welcome Daniel Kirumbi
+inp = input ('Europe floor?)
+             
+
+inp = ('Europe floor?')
+             
+usf = int(inp) + 1
+             
+    usf = int(inp) + 1
+
+inp = input ('European floor ?')
+              
+European floor ?0
+ usf = int(inp) + 1
+             
+ print(' US floor ' , usf 1)
+              
+
+ print ('US floor ?',usf)
+              
+US floor ? 1
+ print (' US floor ',2)
+              
+ US floor  2
+ ('Europe floor', 3)
+             
+('Europe floor', 3)
+ # Find the most common word
+if x > 20 :
+           print ('Bigger')
+ print ('Finis')
+            
+ x = 5
+               
+ if x < 10:
+              print ('Smaller')
+ if x > 20 :
+              
+
+ if x > 20 :
+              print ('Bigger')
+              x > 23
