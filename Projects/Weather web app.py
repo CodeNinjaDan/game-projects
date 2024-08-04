5821977f0818513d6ac59ed5c1bb58d74e2638d8
CodeNinjaDan <dnlmuiru@gmail.com>
2024-08-04 17:29:10 +0300

    Additions

--- Projects/Weather web app.py
+++ Projects/Weather web app.py
@@ -8,7 +8,11 @@ city = input('Enter a city: ')
 
 base_url = f'https://api.openweathermap.org/data/2.5/weather?q={city}&appid={API_Key}'
 
-weather_data = requests.get(base_url).json()
 
-pprint(weather_data)
+weather_data = requests.get(base_url)
 
+if weather_data.status_code == 200:
+    weather = weather_data.json()
+    pprint(weather)
+else:
+    print('Error: Could not retrieve data')
