11a9a70df1bf88a746f695e4889637e5c0c746fe
CodeNinjaDan <dnlmuiru@gmail.com>
2024-10-11 20:04:55 +0300

    Day 14 Changes

--- 100 Days of Python/Higher Lower Game/main.py
+++ 100 Days of Python/Higher Lower Game/main.py
@@ -1,8 +1,6 @@
 import random
-
-from orca.orca import start
-
 from game_data import data
+from art import logo, vs
 #1. Art
 #2. Function -> Randomly choose characters from dict inside a list in game_data
 #       -> Display the chosen characters as A and B
@@ -26,8 +24,9 @@ from game_data import data
 def guess():
     pick_a = random.choice(data)
     follower_values_a = pick_a['follower_count']
+    print(logo)
     print(f"Compare A: {pick_a['name']}, a {pick_a['description']}, from {pick_a['country']}")
-
+    print(vs)
     pick_b = random.choice(data)
     while pick_b == pick_a:
         pick_b = random.choice(data)
@@ -35,24 +34,37 @@ def guess():
     print(f"Against B: {pick_b['name']}, a {pick_b['description']}, from {pick_b['country']}")
     score = 0
     start_game = True
-    user_answer = input("Who has more followers? Type 'A' or 'B': ").upper()
+
     while start_game:
+        user_answer = input("Who has more followers? Type 'A' or 'B': ").upper()
         if user_answer == "A" and follower_values_a > follower_values_b:
             pick_a = pick_b
             follower_values_a = follower_values_b
             pick_b = random.choice(data)
             while pick_b == pick_a:
                 pick_b = random.choice(data)
+            follower_values_b = pick_b['follower_count']
             score += 1
+            print(f"You're right! Current score: {score}")
+            print(f"Compare A: {pick_a['name']}, a {pick_a['description']}, from {pick_a['country']}")
+            print(vs)
+            print(f"Against B: {pick_b['name']}, a {pick_b['description']}, from {pick_b['country']}")
+
         elif user_answer == "B" and follower_values_b > follower_values_a:
             pick_a = pick_b
             follower_values_a = follower_values_b
             pick_b = random.choice(data)
             while pick_b == pick_a:
                 pick_b = random.choice(data)
+            follower_values_b = pick_b['follower_count']
             score += 1
+            print(f"You're right! Current score: {score}")
+            print(f"Compare A: {pick_a['name']}, a {pick_a['description']}, from {pick_a['country']}")
+            print(vs)
+            print(f"Against B: {pick_b['name']}, a {pick_b['description']}, from {pick_b['country']}")
+
         else:
-            print(f"Sorry, that's wrong. score: {score}")
+            print(f"Sorry, that's wrong. Your final score is: {score}")
             start_game = False
 
 guess()
