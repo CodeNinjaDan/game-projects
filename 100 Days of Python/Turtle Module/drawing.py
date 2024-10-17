b5487a17b7d5464674dc2d3e6d29a19289feeccb
CodeNinjaDan <dnlmuiru@gmail.com>
2024-10-17 23:24:05 +0300

    18. Art Drawing

--- 100 Days of Python/Turtle Module/drawing.py
+++ 100 Days of Python/Turtle Module/drawing.py
@@ -1,7 +1,7 @@
 import colorgram
 
 # Extract colors from the image
-colors = colorgram.extract('/home/dan/Desktop/PYTHON/100 Days of Python/Turtle Module/image.jpg', 20)
+colors = colorgram.extract('/home/dan/Desktop/PYTHON/100 Days of Python/Turtle Module/image.jpg', 30)
 
 # Initialize an empty list to store the RGB tuples
 colors_list = []
@@ -9,8 +9,11 @@ colors_list = []
 # Iterate over the extracted colors
 for color in colors:
     # Extract the RGB values and append them to the list
-    rgb = color.rgb
-    colors_list.append((rgb.r, rgb.g, rgb.b))
+    r = color.rgb.r
+    g = color.rgb.g
+    b = color.rgb.b
+    new_color = (r, g, b)
+    colors_list.append((new_color))
 
 # Print the list of RGB tuples
 print(colors_list)
--- 100 Days of Python/Turtle Module/main.py
+++ 100 Days of Python/Turtle Module/main.py
@@ -2,8 +2,10 @@ import random
 from turtle import Turtle, Screen
 from data import colors
 jon = Turtle()
+screen = Screen()
+screen.colormode(255)
 jon.shape("arrow")
-jon.pensize(1)
+# jon.pensize(1)
 jon.speed(10)
 
 
@@ -28,10 +30,41 @@ jon.speed(10)
 # random_move(jon)
 
 #Draw a Spirograph
-for _ in range(72):
-    jon.color(random.choice(colors))
-    jon.circle(100, 360, 100)
-    jon.right(5)
+# for _ in range(72):
+#     jon.color(random.choice(colors))
+#     jon.circle(100, 360, 100)
+#     jon.right(5)
+colours_list = [(205, 160, 84), (57, 87, 130), (145, 91, 42), (138, 27, 49), (221, 207, 110), (135, 175, 200), (155, 50, 85), (44, 55, 104), (167, 159, 42), (130, 188, 145), (83, 21, 43), (190, 139, 162), (38, 42, 65), (184, 94, 107), (87, 120, 178), (59, 38, 29), (88, 156, 93), (81, 151, 165), (194, 83, 71), (54, 127, 120), (78, 76, 44), (46, 75, 78), (45, 75, 73), (163, 200, 216), (217, 176, 188), (220, 182, 168)]
+# loops = 0
+# jon.home()
+# for _ in range(100):
+#     loops += 1
+#     color_choice = random.choice(colours_list)
+#     jon.dot(20, color_choice)
+#     jon.penup()
+#     jon.hideturtle()
+#     jon.forward(50)
+#     jon.showturtle()
+#     jon.pendown()
+#     if loops % 10 == 0:
+#         jon.setx(0)
+#         jon.sety(jon.ycor() + 50)
+#         jon.setheading(0)
 
-screen = Screen()
+jon.penup()
+jon.hideturtle()
+jon.goto(-200, 200)  # Start position
+
+for i in range(100):
+    color_choice = random.choice(colours_list)
+    jon.dot(20, color_choice)
+    jon.forward(50)
+
+    # Turn right and move to the next row after every 10 dots
+    if (i + 1) % 10 == 0:
+        jon.right(90)
+        jon.forward(50)
+        jon.right(90)
+        jon.forward(500)
+        jon.right(180)
 screen.exitonclick()
