7b957f839f2061316bcdaf8f9a647f3c90e5e0e7
CodeNinjaDan <dnlmuiru@gmail.com>
2024-10-26 11:26:11 +0300

    Added Scoreboard functionality

--- Turtle Crossing Game/scoreboard.py
+++ Turtle Crossing Game/scoreboard.py
@@ -1,12 +1,24 @@
-FONT = ("Courier", 24, "normal")
+FONT = ("Courier", 30, "normal")
 from turtle import Turtle
 
 class Scoreboard(Turtle):
 
     def __init__(self):
         super().__init__()
+        self.color("black")
+        self.penup()
+        self.hideturtle()
+        self.score = 0
+        self.update_scoreboard()
 
+    def update_scoreboard(self):
+        self.clear()
+        self.goto(-200, 250)
+        self.write(self.score, align="center", font=FONT)
 
+    def increase_score(self):
+        self.score += 1
+        self.update_scoreboard()
 
     def increase_level(self):
         pass
